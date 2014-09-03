25e720d8ff2bd138ce7259a205e08cf4f95e3abf
Jamie Ingram <jamie@pokelondon.com>
2014-09-03 09:55:53 +0100

    navigation tweaks

--- buildStatic.sh
+++ buildStatic.sh
@@ -3,8 +3,15 @@
 # clean build directories
 rm -rf build/monstersupplies/
 
-# grab mirror of staging sites
+# grab mirror of staging site
 wget --mirror --convert-links -nH --page-requisites -P ./build/monstersupplies https://monster-supplies.myshopify.com/
+
+#copy assets directory from project
+cp src/theme/master/assets build/monstersupplies
+
+#get compiled css and js
+wget -nd -O=src/theme/master/assets/theme.scss.css https://monster-supplies.myshopify.com/assets/theme.scss.css
+wget 
 
 # correct mangled font paths in build css
 #sed -i -e s#http://stage.goldsmiths.pokedev.net/static/fonts/#../../fonts/#g build/goldsmiths/static/css/gs/main.css
